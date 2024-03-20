a96d1a511c7b372a612d0c931f614c44b92ab131
Sailaja Panja <sailaja.panja@gmail.com>
2024-03-20 21:02:39 +0000

    refactored and added comments to the file

--- alternative.py
+++ alternative.py
@@ -1,27 +1,46 @@
+"""
+First part of this task takes a string from the user
+and makes each alternative character upper and lower
+case
+"""
+string_value = input("Please enter a string: ")
+# initialise an empty string to hold final string
+upper_lower_string = ""
 
-string_value = input("Please enter a string : ")
-string_value_new = " "
-
+# iterate through the word, from 0 to word length
 for num in range(len(string_value)):
+    # if the number if divisible by 2, its even number
     if (num % 2 == 0):
-        string_value_new += string_value[num].lower()
+        # lower the letter and concatenate
+        upper_lower_string += string_value[num].lower()
     else:
-        string_value_new += string_value[num].upper()
-print(string_value_new)       
-           
-string_value = input("Please enter a string : ") 
-string_value_1 = string_value.split()    
+        # alternatively, upper the letter and concatenate
+        upper_lower_string += string_value[num].upper()
+print(upper_lower_string)       
 
-print(string_value_1)
+"""
+Second part of this task takes a sentence from the user
+and makes each alternative word lower and upper
+case
+"""
 
-for num in range (len(string_value_1)):
+sentence = input("Please enter a sentence: ")
+# split the words in the sentence into a list
+words_list = sentence.split()
+print(words_list)
+
+# for each word's index,
+for num in range (len(words_list)):
+    # if its odd numbered word, make it upper
     if num % 2 != 0:
-        string_value_1[num] = string_value_1[num].upper()
+        words_list[num] = words_list[num].upper()
         num += 1
     else:
-        string_value_1[num] = string_value_1[num].lower()
+        # even numbered word to be lower
+        words_list[num] = words_list[num].lower()
 
-print(string_value_1)
-string_value_1 = " ".join(string_value_1) 
-print(string_value_1)  
+print(words_list)
+# join them together and print
+lower_upper_sentence = " ".join(words_list) 
+print(lower_upper_sentence)  
      
