d30b4f4a3ccff04b3baea8fd049f3efd66701c65
Sailaja Panja <sailaja.panja@gmail.com>
2024-03-20 21:13:33 +0000

    add comments to age-quiz.py

--- age-quiz.py
+++ age-quiz.py
@@ -1,13 +1,20 @@
+# ask user for age
 age = int(input("enter your age :"))
+# if less than 13, qualified for the kiddie discount
 if age < 13:
     print("you are qualified for the kiddie discount")
+# if age is 21, congratulate
 elif age ==21:
-    print("congrats onyour 21st!")
+    print("congrats on your 21st!")
+# if age is more than 65 but less than 100, retired
 elif age >= 65:
     print("Enjoy your retirement!")
+# if age more than 40 but less than 65, they are over the hill
 elif age >= 40:
     print("you are over the hill.")
+# if more than 100, good for them
 elif age > 100:
     print("sorry, you are dead.")
+# cover everything else
 else:
     print("Age is but a number")
